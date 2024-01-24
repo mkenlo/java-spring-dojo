e5b205d382feec37e8116d2fe8bf14c875276cdb
Melanie <mel.lontsi@gmail.com>
2024-01-23 20:20:34 -0800

    added 2 new routes with Path parameters

--- daikichiroutes/src/main/java/com/mkenlo/daikichiroutes/DaikichiController.java
+++ daikichiroutes/src/main/java/com/mkenlo/daikichiroutes/DaikichiController.java
@@ -1,5 +1,6 @@
 package com.mkenlo.daikichiroutes;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +20,17 @@ public class DaikichiController {
     @RequestMapping("tomorrow")
     public String tomorrow(){
         return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas";
+    }
+
+    @RequestMapping("/travel/{destination}")
+    public String travelTo(@PathVariable String destination){
+        return "Congratulations! You will soon travel to "+destination;
+    }
+
+    @RequestMapping("/lotto/{lottoNumber}")
+    public String winLotto(@PathVariable int lottoNumber){
+        if(lottoNumber%2==0)
+            return "You will take a grand journey in the near future, but be weary of tempting offers";
+        else return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.";
     }
 }

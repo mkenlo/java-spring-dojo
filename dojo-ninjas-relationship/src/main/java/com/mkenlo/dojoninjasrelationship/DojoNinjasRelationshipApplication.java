9342c25d098442f8645ee2df5d8d8f23e5ee8705
Melanie <mel.lontsi@gmail.com>
2024-02-06 12:03:05 -0800

    project setup

---
+++ dojo-ninjas-relationship/src/main/java/com/mkenlo/dojoninjasrelationship/DojoNinjasRelationshipApplication.java
@@ -0,0 +1,13 @@
+package com.mkenlo.dojoninjasrelationship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DojoNinjasRelationshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DojoNinjasRelationshipApplication.class, args);
+	}
+
+}
---
+++ dojo-ninjas-relationship/src/test/java/com/mkenlo/dojoninjasrelationship/DojoNinjasRelationshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mkenlo.dojoninjasrelationship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DojoNinjasRelationshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

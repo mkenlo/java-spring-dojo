1945bdcf11c7eeb626f51d1fab49f6bd7d8fae08
Melanie <mel.lontsi@gmail.com>
2024-02-09 23:54:20 -0800

    added app routes

--- book-club/src/main/java/com/mkenlo/bookclub/controllers/BookController.java
+++ book-club/src/main/java/com/mkenlo/bookclub/controllers/BookController.java
@@ -1,8 +1,77 @@
 package com.mkenlo.bookclub.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.mkenlo.bookclub.models.Book;
+import com.mkenlo.bookclub.services.BookService;
+import com.mkenlo.bookclub.services.UserService;
+
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @Controller
+@RequestMapping("/books")
 public class BookController {
+
+    @Autowired
+    private BookService bookService;
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("")
+    public String index(HttpSession session, Model model, RedirectAttributes redirect) {
+        if (session.getAttribute("userId") == null) {
+            redirect.addFlashAttribute("loginRequired", "Sorry, you need to login before.");
+            return "redirect:/";
+        }
+
+        long userId = (Long) session.getAttribute("userId");
+
+        model.addAttribute("user", userService.findById(userId));
+        List<Book> books = bookService.getAllBooks();
+        model.addAttribute("books", books);
+        return "dashboard.jsp";
+    }
+
+    @GetMapping("/{bookId}")
+    public String showBookDetail(@PathVariable("bookId") Long id, Model model) {
+        Book book = bookService.getABook(id);
+        model.addAttribute("book", book);
+        return "book-detail.jsp";
+    }
+
+    @PostMapping("/new")
+    public String addABook(@Valid @ModelAttribute Book book, BindingResult result) {
+        return "redirect:/books";
+    }
+
+    @GetMapping("/new")
+    public String showNewbookForm() {
+        return "book-add.jsp";
+    }
+
+    @GetMapping("/{bookId}/edit")
+    public String showBookToEdit(@PathVariable("bookId") long id) {
+        return "book-edit.jsp";
+    }
+
+    @PutMapping("/{bookId}/edit")
+    public String updateABook(@PathVariable("bookId") long id) {
+        return "redirect:/books";
+    }
 
 }
--- book-club/src/main/java/com/mkenlo/bookclub/controllers/LoginController.java
+++ book-club/src/main/java/com/mkenlo/bookclub/controllers/LoginController.java
@@ -1,8 +1,64 @@
 package com.mkenlo.bookclub.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.mkenlo.bookclub.models.LoginUser;
+import com.mkenlo.bookclub.models.User;
+import com.mkenlo.bookclub.services.UserService;
+
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 
 @Controller
 public class LoginController {
+    @Autowired
+    UserService userService;
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("newUser", new User());
+        model.addAttribute("newLogin", new LoginUser());
+        return "index.jsp";
+    }
+
+    @PostMapping("/login")
+    public String doLogin(@Valid @ModelAttribute("newLogin") LoginUser user, BindingResult result, Model model,
+            HttpSession session) {
+
+        User loggedUser = userService.doLogin(user, result);
+        if (result.hasErrors()) {
+            model.addAttribute("newLogin", new LoginUser());
+            return "index.jsp";
+        }
+
+        model.addAttribute("user", loggedUser);
+        session.setAttribute("userId", loggedUser.getId());
+        return "redirect:/books";
+    }
+
+    @PostMapping("/register")
+    public String register(@Valid @ModelAttribute("newUser") User user, BindingResult result, HttpSession session,
+            Model model) {
+
+        User newUser = userService.createUser(user, result);
+        if (result.hasErrors()) {
+            model.addAttribute("newLogin", new LoginUser());
+            return "index.jsp";
+        }
+        session.setAttribute("userId", newUser.getId());
+        return "redirect:/books";
+    }
+
+    @GetMapping("/logout")
+    public String doLogout(HttpSession session) {
+        session.removeAttribute("userId");
+        return "redirect:/";
+    }
 
 }
